3ff9f2038059326d731ebd24f7f2459360adfdf0
tessawiedmann <tlwiedmann@gmail.com>
2019-03-31 18:54:57 -0700

    Tessa | Files for custom blocks

---
+++ tessa_test/custom_blocks.js
@@ -0,0 +1,28 @@
+Blockly.JavaScript['music_lucy'] = function(block) {
+  var dropdown_notes = block.getFieldValue('notes');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['music_geoff'] = function(block) {
+  var dropdown_notes = block.getFieldValue('notes');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['music_freddie'] = function(block) {
+  var dropdown_notes = block.getFieldValue('notes');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['loops_repeat'] = function(block) {
+  var dropdown_repeat = block.getFieldValue('repeat');
+  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
